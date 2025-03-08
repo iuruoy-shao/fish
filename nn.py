6d478039473f7b2a4070d3b39d3c37a6942f1324
iuruoy-shao <youruishao115022@gmail.com>
2025-03-07 22:02:52 -0800

    activation functions

--- nn.py
+++ nn.py
@@ -18,10 +18,16 @@ class QNetwork(nn.Module):
         self.pick_person = nn.Linear(64, 4)
         self.pick_ask_set = nn.Linear(64 + 4, 9)
         self.pick_ask_card = nn.Linear(64 + 4 + 9, 6)
+
+        self.dropout = nn.Dropout(0.2)
         
     def forward(self, x, action_masks):
         x = torch.flatten(x, 1)
-        x = self.fc3(self.fc2(self.fc1(x)))
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc3(x))
         to_call = self.to_call(x)
         call_set = self.pick_call_set(x)
         call_cards = torch.reshape(self.pick_call_cards(torch.cat((x, call_set), 1)), (-1, 6, 4))
