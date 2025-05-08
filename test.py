fc7e8736dffc742766977b11e9524f59a86f5998
Yourui Shao <youruishao115022@gmail.com>
2025-05-08 11:31:13 -0700

    testing code complete

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-from verify_data import FishGame
+from verify_data import SimulatedFishGame
 from agent import QLearningAgent
 import numpy as np
+import copy
 
-player = "P1"
+np.set_printoptions(suppress=True)
+player = "YS"
 
 with open("data/test.txt", "r") as f:
     lines = f.readlines()
-game = FishGame(lines)
-
+game = SimulatedFishGame(8)
+game.datarows = lines
+game.init_hands = {player.split(":")[0]:set(player.split(":")[1][1:-1].split(',')) 
+                           for player in game.datarows[0].split()}
+game.hands = [game.init_hands]
+game.players = list(game.init_hands.keys())
 agent = QLearningAgent()
 agent.load_model('models/fish_agent.pth')
 
@@ -16,4 +22,8 @@ game_state = game.to_state()
 state = agent.tensor(np.stack([game.get_state(i, game_state) for i in range(len(game.hands))]))
 mask = agent.action_masks(*agent.unpack_memory([game.mask_dep(len(game.hands)-1, player)]).values())
 pred_hands, action = agent.act(state, mask)
-print(action)
+new_hands = copy.deepcopy(game.hands[-1])
+if action['call'][0] > action['call'][1]:
+    print(game.handle_call(action, new_hands, player))
+else:
+    print(game.handle_ask(action, new_hands, player))
