7267e02c91581ec7c35c240522b0eb5a9e315c1a
Yourui Shao <youruishao115022@gmail.com>
2025-05-09 09:55:10 -0700

    ultimate model overhaul

--- train.py
+++ train.py
@@ -1,10 +1,15 @@
 from agent import QLearningAgent
 import pickle
 
-# with open('memories_extended.pkl', 'rb') as f:
-#     memories = pickle.load(f)
+with open('memories_extended.pkl', 'rb') as f:
+    memories = pickle.load(f)
+with open('call_memories.pkl', 'rb') as f:
+    call_memories = pickle.load(f)
+with open('stored_memories.pkl', 'rb') as f:
+    stored_memories = pickle.load(f)
 
 agent = QLearningAgent()
-agent.load_model('models/fish_agent.pth')
-# agent.train_on_data(memories[::25], hand_epochs=0, q_epochs=10)
-agent.train_self_play(100, hand_epochs=5, q_epochs=5, path='models/fish_agent.pth')
+agent.train_on_data(stored_memories + call_memories, 20, 0)
+agent.save_model('models/fish_agent3.pth')
+agent.train_on_data(memories, 0, 25)
+agent.save_model('models/fish_agent3.pth')
