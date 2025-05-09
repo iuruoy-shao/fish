bd6323f154d268906cdc2153dfe796894770ea2b
Yourui Shao <youruishao115022@gmail.com>
2025-05-08 19:01:34 -0700

    tactical training

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from agent import QLearningAgent
 import pickle
 
-with open('memories_extended.pkl', 'rb') as f:
-    memories = pickle.load(f)
+# with open('memories_extended.pkl', 'rb') as f:
+#     memories = pickle.load(f)
 
-agent = QLearningAgent(memories)
+agent = QLearningAgent()
 agent.load_model('models/fish_agent.pth')
-agent.train_self_play(300, hand_epochs=5, q_epochs=5, path='models/fish_agent.pth')
+# agent.train_on_data(memories[::25], hand_epochs=0, q_epochs=10)
+agent.train_self_play(100, hand_epochs=5, q_epochs=5, path='models/fish_agent.pth')
