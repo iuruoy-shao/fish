04662be3083c96ae49addbac7ce0f6f67ee4da39
Yourui Shao <youruishao115022@gmail.com>
2025-05-09 17:07:46 -0700

    the better model?

--- agent.py
+++ agent.py
@@ -27,32 +27,26 @@ class HandPrediction(nn.Module):
 class QNetwork(nn.Module):
     def __init__(self):
         super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(8*54, 256)
-        self.fc2 = nn.Linear(256, 64)
         self.dropout = nn.Dropout(0.5)
-
-        self.to_call = nn.Linear(64, 2)
-        self.pick_call_set = nn.Linear(64, 9)
-        self.pick_call_cards = nn.Linear(64 + 9, 24)
-        
-        self.fc3 = nn.Linear(8*54, 4*54)
-        self.ask = nn.Linear(4*54, 4*54)
+        self.fc1 = nn.Conv2d(1, 1, (8, 6), stride=(1, 6), padding=0)
+        self.to_call = nn.Linear(9, 2)
+        self.pick_call_set = nn.Linear(9, 9)
+        self.pick_call_cards = nn.Linear(8, 4)
+        self.ask = nn.Linear(8, 4)
 
     def forward(self, x, action_masks):
-        x = x.reshape(-1, 8*54)
-        x1 = self.dropout(F.relu(self.fc1(x)))
-        x1 = self.dropout(F.relu(self.fc2(x1)))
-
+        x1 = x.reshape(-1, 1, 8, 54)
+        x1 = F.relu(self.fc1(x1).reshape(-1, 9))
         to_call = self.to_call(x1)
-        call_set = self.pick_call_set(x1)
-        call_cards = torch.reshape(self.pick_call_cards(torch.cat((x1, call_set), dim=1)), (-1, 6, 4))
-
-        x2 = self.dropout(F.relu(self.fc3(x)))
-        ask = self.ask(x2)
-
+        call_set = self.pick_call_set(x1).masked_fill(~action_masks['call_set'], -1e9)
+        sets = torch.argmax(call_set, dim=1)
+        x = x.reshape(-1, 8, 9, 6)
+        call_cards = self.pick_call_cards(x.permute(2, 0, 3, 1)[sets, torch.arange(x.shape[0])])
+        ask = self.ask(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2).reshape(-1, 4*54)
+        
         return {
             'call': to_call,
-            'call_set': call_set.masked_fill(~action_masks['call_set'], -1e9),
+            'call_set': call_set,
             'call_cards': call_cards.masked_fill(~action_masks['call_cards'], -1e9),
             'ask': ask.masked_fill(~action_masks['ask'], -1e9)
         }
@@ -318,9 +312,9 @@ class QLearningAgent:
 
     def train_self_play(self, n_games, q_epochs=10, hand_epochs=10, path='models/model.pth'):
         try:
-            with open('stored_memories.pkl', 'rb') as f:
+            with open('stored_memories_2.pkl', 'rb') as f:
                 memories = pickle.load(f)
-            with open('call_memories.pkl', 'rb') as f:
+            with open('call_memories_2.pkl', 'rb') as f:
                 call_memories = pickle.load(f)
         except (FileNotFoundError, EOFError):
             memories = self.real_data
@@ -336,8 +330,8 @@ class QLearningAgent:
                     self.train_on_data(random.sample(memories, 100), q_epochs*3, hand_epochs*3, lr_schedule=False)
                 if len(call_memories) > 100:
                     self.train_on_data(random.sample(call_memories, 100), q_epochs*3, 0, lr_schedule=False)
-                self.pickle_memory(memories, 'stored_memories.pkl')
-                self.pickle_memory(call_memories, 'call_memories.pkl')
+                self.pickle_memory(memories, 'stored_memories_2.pkl')
+                self.pickle_memory(call_memories, 'call_memories_2.pkl')
                 self.save_model(path)
 
     def simulate_game(self):
--- train.py
+++ train.py
@@ -1,15 +1,13 @@
 from agent import QLearningAgent
 import pickle
 
-with open('memories_extended.pkl', 'rb') as f:
-    memories = pickle.load(f)
-with open('stored_memories.pkl', 'rb') as f:
-    stored_memories = pickle.load(f)
+# with open('memories_extended.pkl', 'rb') as f:
+#     memories = pickle.load(f)
+# with open('stored_memories.pkl', 'rb') as f:
+#     stored_memories = pickle.load(f)
 
-print(f"memories: {len(memories)}, stored_memories: {len(stored_memories)}")
+# print(f"memories: {len(memories)}, stored_memories: {len(stored_memories)}")
 
 agent = QLearningAgent()
-agent.train_on_data(stored_memories, 20, 0)
-agent.save_model('models/fish_agent3.pth')
-agent.train_on_data(memories, 0, 25)
-agent.save_model('models/fish_agent3.pth')
+agent.load_model('models/model.pth')
+agent.train_self_play(100,5,5,'models/model.pth')
